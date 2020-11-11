45187638ab41ce882cdbaedb92c16494675f4e57
Alan Gomes <alangf@gmail.com>
2020-11-11 15:07:58 -0300

    links sample product page & not found page on home

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="container mx-auto pt-4">
       <Head>
         <title>Habitest</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-
+        <h1 className="text-3xl mb-4">Habitest</h1>
+        <Link href="/p/resurrection-conditioner">
+          Sample product
+        </Link>
+        <br/>
+        <Link href="/p/x">
+          Non existant product
+        </Link>
       </main>
     </div>
   )
